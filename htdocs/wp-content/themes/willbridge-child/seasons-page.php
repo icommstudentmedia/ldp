b78335b55ce4745569234e97c4d3ab0f327bac6e
legoracer18 <legoracer18@gmail.com>
2014-07-01 17:04:19 -0600

    Changed around some of the files for the child themes to get the Seasons pages showing up correctly.

---
+++ htdocs/wp-content/themes/willbridge-child/seasons-page.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Template Name: Seasons Page
+ */
+
+get_header(); 
+
+$season_start = htmlspecialchars($_GET["seasonstart"]);
+$season_end = htmlspecialchars($_GET["seasonend"]);
+
+$seasons = array();
+
+if ($season_end > $season_start) {
+   for ($i = $season_start; $i <= $season_end; $i++) { 
+      array_push($seasons, "Season ".$i);
+   }
+
+
+   foreach ($seasons as $single_season) {
+      $args = array(
+      'post_type' => 'post',
+      'orderby' => 'date',
+      'order' => 'ASC', 
+      'category_name' => $single_season );
+      ?>
+      <div class="season_blocks">
+         <h1><?php echo $single_season ?></h1>
+         <?php
+         $myposts = new WP_Query($args);
+         if($myposts->have_posts()) :
+            while ($myposts->have_posts()) {
+               $myposts->the_post();
+               ?>
+               <figure class="seasons_thumbs">
+                  <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+                  <figcaption><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></figcaption>
+               </figure>
+               <?php
+            }
+         endif;
+         ?>
+      </div>
+      <?php 
+   }
+} else {
+   ?>
+   <h1><a href="http://localhost/ldp/htdocs/seasons/?seasonstart=1&amp;seasonend=5">Seasons 1-5</a></h1>
+   <h1><a href="http://localhost/ldp/htdocs/seasons/?seasonstart=6&amp;seasonend=10">Seasons 6-10</a></h1>
+   <h1><a href="http://localhost/ldp/htdocs/seasons/?seasonstart=11&amp;seasonend=15">Seasons 11-15</a></h1>
+   <h1><a href="http://localhost/ldp/htdocs/seasons/?seasonstart=16&amp;seasonend=20">Seasons 16-20</a></h1>
+   <h1><a href="http://localhost/ldp/htdocs/seasons/?seasonstart=21&amp;seasonend=21">Season 21</a></h1>
+   <?php
+}
+
+
+get_footer();
+?>
---
+++ htdocs/wp-content/themes/willbridge-child/sidebar.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Willbridge
+ * @since Willbridge 1.0
+ * 
+ * Sidebar Template
+ * Created by CMSMasters
+ * 
+ */
+
+
+if (function_exists('generated_dynamic_sidebar_cmsmasters')) {
+   
+   generated_dynamic_sidebar_cmsmasters(1);
+   echo '<!-- This is the Social Media Links -->
+         <ul class="social_list"><li><a target="_blank" href="https://twitter.com/ldprofiles" title="Twitter"><img src="http://www.latterdayprofiles.org/wp-content/themes/willbridge/images/social_icons/twitter.png" alt="twitter" /></a></li><li><a target="_blank" href="https://www.facebook.com/pages/Latter-day-Profiles/187329654624001" title="Facebook"><img src="http://www.latterdayprofiles.org/wp-content/themes/willbridge/images/social_icons/facebook.png" alt="facebook" /></a></li></ul><br>';
+} else {
+    echo '<aside class="widget widget_search">';
+    
+    get_search_form();
+    
+    echo '</aside';
+}
+
+?>
