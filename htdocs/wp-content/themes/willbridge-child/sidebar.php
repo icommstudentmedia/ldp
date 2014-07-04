7d26d4e4f655ac6b181774469e4902a3850060f9
legoracer18 <legoracer18@gmail.com>
2014-07-04 03:01:03 -0600

    Made some changes to the CSS.

--- htdocs/wp-content/themes/willbridge-child/sidebar.php
+++ htdocs/wp-content/themes/willbridge-child/sidebar.php
@@ -14,7 +14,7 @@ if (function_exists('generated_dynamic_sidebar_cmsmasters')) {
    
    generated_dynamic_sidebar_cmsmasters(1);
    echo '<!-- This is the Social Media Links -->
-         <ul class="social_list"><li><a target="_blank" href="https://twitter.com/ldprofiles" title="Twitter"><img src="http://www.latterdayprofiles.org/wp-content/themes/willbridge/images/social_icons/twitter.png" alt="twitter" /></a></li><li><a target="_blank" href="https://www.facebook.com/pages/Latter-day-Profiles/187329654624001" title="Facebook"><img src="http://www.latterdayprofiles.org/wp-content/themes/willbridge/images/social_icons/facebook.png" alt="facebook" /></a></li></ul><br>';
+         <ul class="social_list"><li><a target="_blank" href="https://twitter.com/ldprofiles" title="Twitter"><img src="http://www.latterdayprofiles.org/wp-content/themes/willbridge-child/images/social_icons/twitter.png" alt="twitter" /></a></li><li><a target="_blank" href="https://www.facebook.com/pages/Latter-day-Profiles/187329654624001" title="Facebook"><img src="http://www.latterdayprofiles.org/wp-content/themes/willbridge-child/images/social_icons/facebook.png" alt="facebook" /></a></li></ul><br>';
 } else {
     echo '<aside class="widget widget_search">';
     
